abf5bf4632985f87810a148c804ef160de750a29
leexuan0128 <jypercent@gmail.com>
2021-10-02 12:50:48 +0930

    update backward flow

--- demo.py
+++ demo.py
@@ -29,7 +29,7 @@ def viz(img, flo):
     IO.writeFlow("flowdata/raft_optical_flow.flo", flo)
     # map flow to rgb image
     flo = flow_viz.flow_to_image(flo)
-    img_flo = np.concatenate([img, flo], axis=0)
+    # img_flo = np.concatenate([img, flo], axis=0)
     # import matplotlib.pyplot as plt
     # plt.imshow(img_flo / 255.0)
     # plt.show()
@@ -40,28 +40,45 @@ def viz(img, flo):
     return flo
 
 def epe(all_flow, gt_filename):
-    gt_filename = "GT-F-0400.pfm"
+    # GT imgsave
+    # gt_filename = "GT-F-0400.pfm"
+    # gt_filename = "GT-B-0401.pfm"
     gt_flow = IO.readFlow(gt_filename)
+    #flo = flow_viz.flow_to_image(gt_flow)
+    #plt.imsave("GT_401_back.png", flo)
+
     gt_flow = torch.Tensor([gt_flow])
     gt_flow = gt_flow.permute(0,3,1,2)
     padder = InputPadder(gt_flow.shape)
     gt_flow = padder.pad(gt_flow)[0]
     gt_flow = gt_flow.numpy()
+
     all_epe = []
     for flow in all_flow:
         flow = flow.numpy()
-        epe = np.linalg.norm(flow - gt_flow)
+        #epe = np.mean(np.sum((flow - gt_flow)**2, axis = 1)**0.5) # Euclidean distance 
+        epe = np.sum((flow - gt_flow)**2, axis = 1)**0.5
         all_epe.append(epe)
+    #np.save("non_rigid_avg_epe_back_15.npy", all_epe)
 
-    import matplotlib.pyplot as plt
-    x = np.arange(1,len(all_epe)+1)
+    # flowList = np.load("non_rigid_avg_epe_back_15.npy")
+    # flow_err = flowList.reshape(544, 960)
+
+    plt.imshow(epe[0], vmin=0, vmax=5)
+    plt.savefig("err.png")
+
+    # draw
+    x = range(15)
     y = all_epe
-    plt.title("{} Iterations without Rigid_flow".format(str(len(all_epe)))) #标题
-    plt.xlabel("Iteration times") #x轴的名称
-    plt.ylabel("End-Point-Error")
+    #y2 = flowList
+    plt.title("{} Iteration all EPE".format(str(len(all_epe)))) 
+    plt.xlabel("Iterations") 
+    plt.ylabel("avg end-point-error")
     plt.grid(True)
-    plt.plot(x,y)
-    plt.savefig("epe/raft-end-point-error-{}.png".format(str(len(all_epe))))
+    plt.plot(x,y, label = "rigid")
+    #plt.plot(x,y2, label = "non-rigid")
+    plt.legend()
+    plt.savefig("epe/rigid_avg_epe_back-{}.png".format(str(len(all_epe))))
     plt.show()
 
 def demo(args):
@@ -76,26 +93,28 @@ def demo(args):
         images = glob.glob(os.path.join(args.path, '*.png')) + \
                  glob.glob(os.path.join(args.path, '*.jpg'))
         
-        flow_init = IO.readFlow("/home/xuanli/RAFT/rigid_flow.flo")
+        flow_init = IO.readFlow("rigid_flow_back.flo")
         flow_init = cv2.resize(flow_init,(120,68), interpolation = cv2.INTER_CUBIC) / 8
         flow_init = flow_init[np.newaxis, :, :, :]
         flow_init = torch.from_numpy(flow_init).permute(0,3,1,2)
 
         images = sorted(images)
         i = 1
-        for imfile1, imfile2 in zip(images[:-1], images[1:]):
+        for imfile1, imfile2 in zip(images[1:], images[:-1]):
             image1 = load_image(imfile1)
             image2 = load_image(imfile2)
 
             padder = InputPadder(image1.shape)
             image1, image2 = padder.pad(image1, image2)
-            flow_low, flow_up, flow_all = model(image1, image2, flow_init=None, iters=15, test_mode=True)
+            flow_low, flow_up, flow_all = model(image1, image2, flow_init, iters=15, test_mode=True)
             flow_image = viz(image1, flow_up)
             # cv2.imwrite("tmp/raft_optical_flow_{}.jpg".format(i), flow_image)
-            plt.imsave("tmp/raft_optical_flow_{}.jpg".format(i), flow_image)
+            plt.imsave("tmp/rigid_optical_flow_back_{}.jpg".format(i), flow_image)
             i = i + 1
-         
-    epe(flow_all, "GT-F-0400.pfm")
+
+    #epe(flow_all, "GT-F-0400.pfm")
+    epe(flow_all, "GT-B-0401.pfm")
+
 
 if __name__ == '__main__':
     parser = argparse.ArgumentParser()
